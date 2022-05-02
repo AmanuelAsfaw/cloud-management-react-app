21c1d64a80e3d4d2255473576b2c57c30fc42fa4
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-02 17:51:23 +0300

    Make a Hero Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
       <h1 className="text-3xl text-center">
         Hello world!
       </h1>
