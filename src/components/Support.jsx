d1f57f4913b9867ca679d42f193a79d6723e3a83
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-03 15:03:56 +0300

    Make All_In_One Component

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -5,7 +5,7 @@ import supportImg from "../assets/support.jpg"
 
 function Support() {
     return (
-        <div className="w-full h-screen mt-24">
+        <div className="w-full mt-24">
             <div className="w-full h-[700px] bg-gray-900/90 absolute">
                 <img className=" w-full h-full object-cover mix-blend-overlay" src={supportImg} alt="/"/>
             </div>
@@ -13,7 +13,7 @@ function Support() {
                 <div className="">
                     <h2 className="text-3xl pt-8 text-slate-300 uppercase text-center">Support</h2>
                     <h3 className="text-5xl font-bold py-6 text-center">Finding the right team</h3>
-                    <p className="py-4 text-3xl text-slate-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
+                    <p className="py-4 text-3xl text-slate-300 xsm:px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. In repudiandae veritatis ratione error tenetur, voluptates architecto possimus ad! Omnis minima ea quidem quisquam unde beatae, minus illo et cum vel?</p>
                 </div>
 
                 <div className=" grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   theme: {
     screens: {
+      xsm: '350px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
