627125eaefc18eae14592d539e3d16804b6a69fe
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-03 15:48:37 +0300

    Make Pricing component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import AllInOne from "./components/AllinOne";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 import Support from "./components/Support";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <About/>
       <Support/>
       <AllInOne/>
+      <Pricing/>
     </div>
   );
 }
